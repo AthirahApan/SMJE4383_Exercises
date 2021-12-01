7c89e16f8284b7aa8626d3820372dbec9feaebd8
AthirahApan <athirahmohdapan-1999@graduate.utm.my>
2021-11-30 23:30:11 -0800

    Exercise16

---
+++ Chap6/Exercise16.py
@@ -0,0 +1,8 @@
+def are_anagrams(word1, word2):
+   word1_sorted = sorted(word1)
+   word2_sorted = sorted(word2)
+   
+   if word1_sorted == word2_sorted:
+      return True
+   else:
+      return False
